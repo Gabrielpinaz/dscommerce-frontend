2aed8aab239e7f29838e859eded2b3e71581ecff
Gabriel Pina <pinagabriel2017@gmail.com>
2023-02-05 12:16:31 -0300

    Context cart count

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,33 +4,37 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import PorductDetailsCard from "../../../components/ProductDetailsCard";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 import * as cartService from "../../../services/cart-service";
+import { ContextCartCount } from "../../../utils/context-cart";
 
 export default function ProductDetails() {
-
   const params = useParams();
 
   const navigate = useNavigate();
 
+  const { setContextCartCount } = useContext(ContextCartCount);
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-      productService.findById(Number(params.productId))
-        .then(response => {
-          console.log(response.data);
-          setProduct(response.data);
-        })
-        .catch(() => {
-          navigate("/");
-        }) 
-  }, [])
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  }, []);
 
   function handleBuyClick() {
     if (product) {
       cartService.addProduct(product);
+      setContextCartCount(cartService.getCart().items.length);
       navigate("/cart");
     }
   }
@@ -38,10 +42,7 @@ export default function ProductDetails() {
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {
-          product &&
-         <PorductDetailsCard product={product} />
-        }
+        {product && <PorductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
           <div onClick={handleBuyClick}>
             <ButtonPrimary text="Comprar" />
