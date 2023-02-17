b8aa2dd2e4791fa3be82cc655ff18a02952560cc
Gabriel Pina <pinagabriel2017@gmail.com>
2023-02-17 11:29:45 -0300

    hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import QueryString from "qs";
-import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { requestBackend } from "../utils/requests";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import * as accessTokenRepository from "../localstorage/access-token-repository";
@@ -54,6 +54,24 @@ export function isAuthenticated(): boolean {
   let tokenPayload = getAccessTokenPayload();
   if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
     return true;
+  }
+  return false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenPayload = getAccessTokenPayload();
+  
+  if (tokenPayload !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+      if (tokenPayload.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+    //return roles.some(role => tokenData.authorities.includes(role));
   }
   return false;
 }
