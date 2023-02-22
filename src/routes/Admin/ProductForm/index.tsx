928e18c3ff8dc813ddf396262b443fa981dec201
Gabriel Pina <pinagabriel2017@gmail.com>
2023-02-22 20:37:56 -0300

    Validation, regex

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -17,6 +17,10 @@ export default function ProductForm() {
       name: "name",
       type: "text",
       placeholder: "Nome",
+      validation: function(value: string) {
+        return /^.{3,80}$/.test(value);
+      },
+      message: "Favvor informar um nome de 3 a 80 caracteres"
     },
     price: {
       value: "",
