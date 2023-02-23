e5b784efd71eddfd2dbf5de9685d95bd07518a72
Gabriel Pina <pinagabriel2017@gmail.com>
2023-02-23 19:21:13 -0300

    React Select integration

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,23 +1,22 @@
 export default function FormInput(props: any) {
+  const {
+    validation,
+    invalid = "false",
+    dirty = "false",
+    onTurnDirty,
+    ...inputProps
+  } = props;
 
-    const { 
-        validation, 
-        invalid = "false", 
-        dirty = "false", 
-        onTurnDirty, 
-        ...inputProps 
-    } = props;
+  function handleBlur() {
+    onTurnDirty(props.name);
+  }
 
-    function handleBlur() {
-        onTurnDirty(props.name);
-    }
-
-    return (
-        <input 
-        { ...inputProps} 
-        onBlur={handleBlur}
-        data-invalid={invalid}  
-        data-dirty={dirty}
-        />
-    );
+  return (
+    <input
+      {...inputProps}
+      onBlur={handleBlur}
+      data-invalid={invalid}
+      data-dirty={dirty}
+    />
+  );
 }
